2472892680b751faced477ced5ff521dbb36fb5b
Hrick-08 <119574039+Hrick-08@users.noreply.github.com>
2024-02-06 17:06:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirectToWebsite(choice) {
+    if (choice === 'choice1') {
+        window.location.href = 'nuhuh.html';
+    } else if (choice === 'choice2') {
+        window.location.href = 'yess.html';
+    }
+}
